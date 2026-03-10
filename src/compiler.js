bec8be6df8a902b7f14dad6ebde58403588da060
Dan Behrman <166764905+DanBehrman-CR@users.noreply.github.com>
2026-03-09 19:54:38 -0500

    revert wrapping log config

--- src/compiler.js
+++ src/compiler.js
@@ -124,7 +124,7 @@ const compileTaskDefinition = (images, task) => ({
         Environment: toEnvironment(task.environment),
         EntryPoint: task.entryPoint,
         Command: task.command,
-        ...{...{LogConfiguration: {
+        LogConfiguration: {
           LogDriver: 'awslogs',
           Options: {
             'awslogs-region': { 'Fn::Sub': '${AWS::Region}' },
@@ -133,8 +133,8 @@ const compileTaskDefinition = (images, task) => ({
             },
             'awslogs-stream-prefix': 'fargate',
           },
-        }},
-        ...task.cloudFormationResource.container},
+        },
+        ...task.cloudFormationResource.container,
       },
       ...task.cloudFormationResource.additionalContainers,
     ],
